117bbad18159b64978ebbb55e6bc7fed3193fea6
takeshiD <goriponikeike55@gmail.com>
2023-09-04 06:52:59 +0900

    fork-exec

--- main.c
+++ main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-int main()
+#include <sys/types.h>
+#include <sys/wait.h>
+int main(int argc, char* argv[])
 {
-    char* cmd[] = {"ls", NULL};
-    execvp(cmd[0], cmd);
-    return 1;
+    if(argc != 3){
+        fprintf(stderr, "Argment is too short.\n");
+        exit(1);
+    }
+    pid_t pid = fork();
+    if(pid < 0)
+    {
+        fprintf(stderr, "Error fork\n");
+        return EXIT_FAILURE;
+    }
+    else if(pid == 0)
+    { // 子プロセス
+        execlp(argv[1], argv[1], argv[2], NULL);
+        fprintf(stderr, "Error exec\n");
+        return EXIT_FAILURE;
+    }
+    else
+    { // 親プロセス
+        int status;
+        waitpid(pid, &status, 0);
+        fprintf(stdout, "pid=%d is completed\n", pid);
+    }
+    return 0;
 }
