266084a26f4930d27bf0b6ba651fe6f467373a59
takeshiD <goriponikeike55@gmail.com>
2023-10-17 23:21:04 +0900

    構文解析の実装

--- main.c
+++ main.c
@@ -347,79 +347,84 @@ void print_commandlist(CommandList* cmdlst)
 
 int main(int argc, char* argv[])
 {
-    if(argc != 2){
-        fprintf(stderr, "Argument is short.\n");
-        exit(EXIT_FAILURE);
-    }
-    TokenList* tknlst = tokenize(argv[1]);
-    print_tokenlist(tknlst);
-    CommandList* cmdlst = parse(tknlst);
-    print_commandlist(cmdlst);
-    // char cmd[1024];
-    // while(1)
-    // {
-    //     printf("$ ");
-    //     fgets(cmd, 1024, stdin);
-    //     char* _c = strchr(cmd, '\n');
-    //     if(_c != NULL){ *_c = '\0';}
-    //     char* p = cmd;
-    //     char* argv[5];
-    //     int argc = 0;
-    //     while(*p) // NULLチェック
-    //     {
-    //         if(*p && 0x21 <= *p && *p <= 0x7e) // 0x21<= <=0x7e: 印字可能文字
-    //         {
-    //             char* head;
-    //             char* end;
-    //             head = p; // 印字可能文字の先頭アドレスを覚える
-    //             end  = p;
-    //             p++;
-    //             while(*p && 0x20 <= *p && *p <= 0x7e)
-    //             {
-    //                 if(0x21 <= *p && *p <= 0x7e)
-    //                 {
-    //                     end = p;
-    //                     p++;
-    //                     continue;
-    //                 }
-    //                 if(0x20 == *p)
-    //                 {
-    //                     p++;
-    //                     break;
-    //                 }
-    //             }
-    //             argv[argc] = (char*)calloc(1, end-head+1);
-    //             strncpy(argv[argc], head, end-head+1);
-    //             argc++;
-    //         }
-    //     }
-    //     argv[argc] = 0;
-    //     for(int i=0; i<argc; i++)
-    //     {
-    //         printf("%s\n", argv[i]);
-    //     }
-    //     pid_t pid = fork();
-    //     if(pid < 0)
-    //     {
-    //         fprintf(stderr, "Error fork\n");
-    //         return EXIT_FAILURE;
-    //     }
-    //     else if(pid == 0)
-    //     { // 子プロセス
-    //         execvp(argv[0], argv);
-    //         fprintf(stderr, "Error exec\n");
-    //         return EXIT_FAILURE;
-    //     }
-    //     else
-    //     { // 親プロセス
-    //         int status;
-    //         waitpid(pid, &status, 0);
-    //         fprintf(stdout, "pid=%d is completed\n", pid);
-    //     }
-    //     for(int i=0; i<argc; i++){
-    //         free(argv[i]);
-    //     }
-    //     memset(cmd, 0, 1024);
+    // if(argc != 2){
+    //     fprintf(stderr, "Argument is short.\n");
+    //     exit(EXIT_FAILURE);
     // }
+    // TokenList* tknlst = tokenize(argv[1]);
+    // print_tokenlist(tknlst);
+    // CommandList* cmdlst = parse(tknlst);
+    // print_commandlist(cmdlst);
+    char cmd[1024];
+    while(1)
+    {
+        printf("$ ");
+        fgets(cmd, 1024, stdin);
+        char* _c = strchr(cmd, '\n');
+        if(_c != NULL){ *_c = '\0';}
+        char* p = cmd;
+        // char* argv[5];
+        // int argc = 0;
+        // while(*p) // NULLチェック
+        // {
+        //     if(*p && 0x21 <= *p && *p <= 0x7e) // 0x21<= <=0x7e: 印字可能文字
+        //     {
+        //         char* head;
+        //         char* end;
+        //         head = p; // 印字可能文字の先頭アドレスを覚える
+        //         end  = p;
+        //         p++;
+        //         while(*p && 0x20 <= *p && *p <= 0x7e)
+        //         {
+        //             if(0x21 <= *p && *p <= 0x7e)
+        //             {
+        //                 end = p;
+        //                 p++;
+        //                 continue;
+        //             }
+        //             if(0x20 == *p)
+        //             {
+        //                 p++;
+        //                 break;
+        //             }
+        //         }
+        //         argv[argc] = (char*)calloc(1, end-head+1);
+        //         strncpy(argv[argc], head, end-head+1);
+        //         argc++;
+
+        //     }
+        // }
+
+        // argv[argc] = 0;
+        // for(int i=0; i<argc; i++)
+        // {
+        //     printf("%s\n", argv[i]);
+        // }
+        TokenList* tknlist = tokenize(cmd);
+        CommandList* cmdlist = parse(tknlist);
+        pid_t pid = fork();
+        if(pid < 0)
+        {
+            fprintf(stderr, "Error fork\n");
+            return EXIT_FAILURE;
+        }
+        else if(pid == 0)
+        { // 子プロセス
+            // execvp(argv[0], argv);
+            execvp(cmdlist->head->next->argv[0], cmdlist->head->next->argv);
+            fprintf(stderr, "Error exec\n");
+            return EXIT_FAILURE;
+        }
+        else
+        { // 親プロセス
+            int status;
+            waitpid(pid, &status, 0);
+            fprintf(stdout, "pid=%d is completed\n", pid);
+        }
+        // for(int i=0; i<argc; i++){
+        //     free(argv[i]);
+        // }
+        memset(cmd, 0, 1024);
+    }
     return 0;
 }
